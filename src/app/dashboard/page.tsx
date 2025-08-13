29e0cf103b638a751a368228703483aeb80d8eb6
codewithsamir <teamofsamir@gmail.com>
2025-08-13 12:13:30 +0545

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,16 @@
 import { getCurrentsUser } from '@/action/auth.action';
 import { getFeebbackByUserId } from '@/action/general.action';
 import Dashboard from '@/components/Dashboard';
-import { redirect } from 'next/navigation';
+
 import React from 'react';
 
 const Page = async () => {
   
 
-  const [user, feedback] = await Promise.all([
-  getCurrentsUser(),
-  (async () => {
-    const user:any = await getCurrentsUser();
-    return getFeebbackByUserId({ userId: user.id });
-  })(),
-]);
+const user = await getCurrentsUser();
+const { id: userId }: any = user;
+const feedback = await getFeebbackByUserId({ userId });
 
-  const {id:userId}:any= user ;
- 
 
   // Check if feedback exists and is non-empty
   const isFeedbackAvailable = !!(feedback && Object.keys(feedback).length > 0);
