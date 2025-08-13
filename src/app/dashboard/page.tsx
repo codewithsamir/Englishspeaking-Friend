35cf8aa85cc26e54941f2434a6c71b1f946b3396
codewithsamir <teamofsamir@gmail.com>
2025-08-13 12:01:34 +0545

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,20 @@
 import { getCurrentsUser } from '@/action/auth.action';
 import { getFeebbackByUserId } from '@/action/general.action';
 import Dashboard from '@/components/Dashboard';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
 const Page = async () => {
   // Fetch current user
   const user = await getCurrentsUser();
+
+  // console.log(user)
+  const isUserAvailable = !!(user && Object.keys(user).length > 0);
+
+  if(!isUserAvailable)   redirect("/")
   
   // Extract userId safely
-  const userId = user?.id ;
+  const {id:userId}:any= user ;
 
   // Fetch feedback for this user
   const feedback = await getFeebbackByUserId({ userId });
